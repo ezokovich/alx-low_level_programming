7695ae59d45f527acd94d246be479f783fb3675a
ezokovich <ezokovichtbb@gmail.com>
2023-03-17 07:54:02 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,16 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() -RAND_Max / 2
-		if(n<0)
-		{
-			printf("%d is negative\n,n");
-		}else if (n == 0)
-		{
-			 printf("%d is zero\n,n")
-		}else 
-		{
-			 printf("%d is positive\n,n");
-		} 
-	         return (0)
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
 }
