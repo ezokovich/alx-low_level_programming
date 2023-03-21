51101ad83a20a2bde5f0b7680619974a02ac7c69
ezokovich <ezokovichtbb@gmail.com>
2023-03-21 07:12:51 -0700

    asci add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
 int main (void)
 {
 	_putchar('_');
-	_putchar(113);
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar(98);
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
