ade355419e01da91f9da9f831931fa28ac37747a
ezokovich <ezokovichtbb@gmail.com>
2023-03-21 07:15:37 -0700

    fix of betty error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,26 @@
-#include "main.h"
-/** 
+/*
+ * File: 0-putchar.c 
+ *
+ */
+
+ #include "main.h" 
+
+ /** 
  * main-print putchar followed by a new line
  * 
  *Return :always 0
  */
-int main (void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-                return(0);
-}
+  int main(void) 
+ {
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
