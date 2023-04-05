24a66a48c5b5fff690dae167580b61046dc250db
ezokovich <ezokovichtbb@gmail.com>
2023-04-05 05:59:14 -0700

    4 bug fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,10 +3,8 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise
- *
  * @y: power
  *
- * 
  * Return: result of the power
  */
 int _pow_recursion(int x, int y)
@@ -17,3 +15,4 @@ int _pow_recursion(int x, int y)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
+
