85c77b68052841ae3f846017508f31a513cbbc63
ezokovich <ezokovichtbb@gmail.com>
2023-04-20 09:02:04 -0700

    function_pointers.h and  0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "function_pointers.h"
+
+/**
+ * print_name - print a string who use pointer func
+ *
+ * @name:   add string
+ *
+ * @f: pointer to function
+ *
+ * Return: nothing
+ *
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
